ba0789239828e00242163f56d33605938047dec2
J. Hassler Thurston <jthurst3@u.rochester.edu>
2015-04-03 23:56:43 -0400

    modify grammar to be in EBNF

--- Parser.java
+++ Parser.java
@@ -6,6 +6,19 @@ import java.util.ArrayList;
 public class Parser {
 
 	public static void main(String[] args) {
+		// tests the parser on example inputs
+		for (int i = 0; i < 1; i++) {
+			Parser p = new Parser();
+			Token[] tokens = new Token[] {new Token("1", TokenType.INTEGER), new Token("+", TokenType.OPERATOR), new Token("1", TokenType.INTEGER)};
+			for (int j = 0; j < tokens.length; j++) {
+				boolean status = p.addAndEvaluate(tokens[j]);
+				if (!status) {
+					System.out.println("Test failed after adding token " + tokens[j] + " on input " + p.currentTokens);
+				} else {
+					System.out.println("successfully added token " + tokens[j] + " on inut " + p.currentTokens);
+				}
+			}
+		}
 		Token t = new Token("+", TokenType.OPERATOR);
 		System.out.println(t);
 	}
