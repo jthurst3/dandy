43f617dbb0a17e39465fcff48462d4bc3635d231
J. Hassler Thurston <jthurst3@u.rochester.edu>
2015-04-03 21:40:25 -0400

    add template for parser

---
+++ Parser.java
@@ -0,0 +1,24 @@
+
+
+
+public class Parser {
+
+
+	/** types of tokens:
+	 * Integers
+	 * Variables and functions
+	 * Operators
+	 	=, ==, +, -, *, /, %
+	 * (, ), {, }, ;
+	 * if, elif, else
+	*/
+
+
+	public Parser() {
+
+	}
+
+
+
+
+}
